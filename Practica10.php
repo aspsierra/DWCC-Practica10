671bb4f504b393892d83cd160ec3b7d723d75ad8
Álvaro Sierra <asp.sierra@gmail.com>
2022-11-03 18:42:47 +0100

    03/11/22

--- Practica10.php
+++ Practica10.php
@@ -1,45 +1,44 @@
 <?php
 
+function borraPosicion($arArchivos, $posicion){
+    foreach ($arArchivos as $key => $prop) {
+        unset($prop[$posicion]);
+        $arArchivos[$key]= $prop;
+    }
+    return $arArchivos;
+}
+
 if (isset($_POST["enviar"])) {
     try {
-        //compruebo si existe ['files'] dentro del array superglobal $_FILES
-        if(!isset(($_FILES['files']['error']))){
-            throw new RuntimeException('Error en el envío del fichero ');
-        }
+        $archivos=$_FILES['files'];
         
+        //compruebo si existe ['files'] dentro del array superglobal $_FILES
+        if(!isset(($archivos['error']))){
+            throw new RuntimeException('Error en el envío de los ficheros');
+        }
+      
         //Como los archivos se reciben en un array, hago un foreach para comprobar que no haya errores
         $errEnvio="";
-        foreach ($_FILES['files']['error'] as $key => $error) {
+        foreach ($archivos['error'] as $key => $error) {
             switch ($error) {
                 case UPLOAD_ERR_OK:
                     break;
-                case UPLOAD_ERR_NO_FILE:                   
+                case UPLOAD_ERR_NO_FILE:
+                    $archivos = borraPosicion($archivos, $key);
                     $errEnvio .= 'No se recibió el archivo '.$key . '<br>';
                     break;
                 case UPLOAD_ERR_INI_SIZE:
-                case UPLOAD_ERR_FORM_SIZE:               
+                case UPLOAD_ERR_FORM_SIZE:     
+                    $archivos = borraPosicion($archivos, $key);
                     $errEnvio .= 'El archivo '.$key.' sobrepasa el tamaño máximo'. '<br>';
                     break;
                 default:
-                     $errEnvio .= 'Error desconocido para el archivo '.$key. '<br>';             
+                    $archivos = borraPosicion($archivos, $key);
+                    $errEnvio .= 'Error desconocido para el archivo '.$key. '<br>';             
                 }                      
-        }      
+        }   
         //para que me muestre los errores individuales los guardo en una variable y luego muestro el error
-        if ($errEnvio != ""){
-            throw new RuntimeException($errEnvio);
-        }
-        
-        //Mismo caso que el anterior pero con el tamaño
-        $errSize = "";        
-        foreach($_FILES["files"]["size"] as $key => $size){
-            if($size > 50000000){
-                $errSize .= 'El tamaño de la imagen '.$key.'es mayor de lo permitido'. '<br>';
-            }
-        }
-        //muestro los errores de cada imagen
-        if($errSize != ""){
-            throw new RuntimeException($errSize);
-        }
+        echo $errEnvio;
         
         //creo una variable que usaré para cpmprobar el Mime type de los ficheros enviados
         $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
@@ -47,7 +46,7 @@ if (isset($_POST["enviar"])) {
         $extension = array();
         
         //compruebo cuales son las extensiones de cada uno de los archivos enviados
-        foreach ($_FILES['files']['tmp_name'] as $key => $fichero) {
+        foreach ($archivos['tmp_name'] as $key => $fichero) {
             $extension[$key]= array_search(finfo_file($fileInfo, $fichero), array('jpg'=>'image/jpeg','png'=>'image/png','gif'=>'image/gif','pdf'=>'application/pdf'));                       
         }
         //cierro el finfo ya que no lo voy a seguir usando
@@ -56,28 +55,25 @@ if (isset($_POST["enviar"])) {
         //si en alguna posición hay un false, quiere decir que el tipo de archivo no se corresponde
         foreach ($extension as $key => $value) {
             if ($value === false){
-                throw new RuntimeException('archivo ' . $key . ' no reconocido');
+                $archivos = borraPosicion($archivos, $key);
+                echo 'archivo ' . $key . ' no reconocido'."<br>";              
             }
         }
         
-        $errDir="";
+        $errDir = "";
         //creo el directorio en caso de que no exista
         if(!file_exists('./practica10/')){
             mkdir('./practica10');
         }
         //subo cada fichero      
-        foreach ($_FILES['files']['tmp_name'] as $key => $fichero) {
+        foreach ($archivos['tmp_name'] as $key => $fichero) {
             $res = move_uploaded_file($fichero, "./practica10/file" . $key . "." . $extension[$key]);
             if(!$res){
-                $errDir .= "El fichero ".$key." no pudo ser cambiado de dirección"+"<br>";
+                echo "El fichero ".$key." no pudo ser cambiado de dirección"."<br>";
+            } else {
+                echo $archivos['name'][$key]. " subido correctamente<br>";
             }
         }
-        if ($errDir!=""){
-            throw new RuntimeException($errDir);
-        }
-        
-        //si todo sale bien, muestro esto
-        echo "Archivos subidos correctamente";
         
     } catch (RuntimeException $ex) {
         echo $ex->getMessage();
@@ -99,7 +95,7 @@ if (isset($_POST["enviar"])) {
             <p>Ficheiros a enviar: </p>
             
             <div>
-                <input type="file" name="files[]" />
+                <input type="file" name="files[]" value="<?php if(isset($_FILES['files']))echo $_FILES['files']['name'][0];?>"/>
             </div>          
             <br>            
             <input name="enviar" type="submit" value="Enviar" /> 
--- main.js
+++ main.js
@@ -1,30 +1,51 @@
+function creaInputs(div){
+    var newInput = document.createElement("input");      
+    div.innerHTML +="<br>";
+    newInput.type="file";
+    newInput.name="files[]";       
+    div.appendChild(newInput);
+}
+
 function main(){
-    var divInputs = document.getElementsByTagName("div")[0];
-    var nInputs = document.getElementsByTagName("input").length
     
-    document.getElementById("agregar").addEventListener("click", evt =>{
-        if(nInputs<=10){
-            var newInput = document.createElement("input");      
-            divInputs.innerHTML +="<br>";
-            newInput.type="file";
-            newInput.name="files[]";       
-            divInputs.appendChild(newInput);
+    var nInputs;
+    var divInputs = document.getElementsByTagName("div")[0];
+    
+    document.getElementById("agregar").addEventListener("click", evt =>{     
+        if(nInputs<10){
+            creaInputs(divInputs)
             nInputs++;
+            localStorage.setItem('nInputs',nInputs);
         } else {
             alert("Número máximo de inputs alcanzado");
     } 
     })
     
     document.getElementById("borrar").addEventListener("click", evt =>{
-        if(nInputs > 2){
+        //var divInputs = document.getElementsByTagName("div")[0];
+        if(nInputs > 1){
             for(let i = 0; i <= 1; i++){
                 divInputs.removeChild(divInputs.lastChild);
             }
             nInputs--;
+            localStorage.setItem('nInputs',nInputs);
+
         } else{
             alert("Número mínimo de inputs alcanzado");
         }              
-    })   
+    })
+    
+    window.addEventListener('load', evt=>{
+        if(localStorage.getItem('nInputs')){
+            nInputs = parseInt(localStorage.getItem('nInputs'));
+            for(let i = 0 ; i < nInputs - 1 ; i++){
+                creaInputs(divInputs);
+            }
+        } else{
+            //resto 1 porque el submit es un imput también
+            nInputs = document.getElementsByTagName("input").length - 1;
+        }       
+    })
 }
 
 main();
